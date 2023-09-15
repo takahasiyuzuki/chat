e99f27654b2ae5ca1a865a4072ff5ce50c78ea7d
test1 <taka.yuzu1127@gmail.com>
2023-09-15 15:22:45 +0900

    モデル変更

--- chatApp/app/Models/Image.php
+++ chatApp/app/Models/Image.php
@@ -4,11 +4,11 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+//use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class Image extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -18,6 +18,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'user_name'
+        'page_id',
+        'image_path'
     ];
 }
